7a78f552a163a2d875e018b0b49243174b1b5c7e
Elena <you@example.com>
2024-07-15 01:59:32 +0300

    добавлены шаблоны

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import vk
 from flask import Flask
-from flask import render_template_string
+from flask import render_template_string, render_template
 import os
 from envparse import env
 env.read_envfile()
@@ -11,33 +11,16 @@ token = env('API_TOKEN')
 def getGroups():
     api = vk.API(access_token=token)
     # получить от вк группы одного пользователя по user_id https://dev.vk.com/ru/method/groups.get
-    allGroups = api.groups.get(user_id=241222399,extended=1,fields='description',count=1000,v=5.199)
-    
+    manId=241222399
+    allGroups = api.groups.get(user_id=manId,extended=1,fields='description',count=1000,v=5.199)
+
     groups=allGroups['items']
-    print(groups)
-    res_string='<H2>сайт</H2>'
-    for group in groups:
-        print('https://vk.com/club', group['id'],'  ',group['name'], sep='')
-        res_string+='<img src=\"'
-        res_string+=group['photo_50']
-        res_string+='\'>'
-        res_string+='<br>'
-        res_string+='<p>'
-        res_string+=group['name']
-        res_string+='</p>'
-        # res_string+='<br>'
-        res_string+='<a href=\">'
-        res_string+='https://vk.com/club'
-        res_string+= str(group['id'])
-        res_string+='\'>https://vk.com/club'
-        res_string+=str(group['id'])
-        res_string+='</a>'
-        res_string+='<br>'
-        res_string+='  <p>'
-        res_string+=str(group.get('description'))
-        res_string+='</p>'
-        res_string+='<br>'
-    return render_template_string(res_string)
+    context={}
+    context['1']= groups # list(filter(lambda item: item.get('country') == 1, groups))
+    context['title']=manId
+    # print(groups)
+    
+    return render_template('getGroups.html', context=context)
 
 @app.route("/mem")
 def getGroupMembers():
@@ -48,36 +31,8 @@ def getGroupMembers():
     groups=allGroups['items']
     girls=list(filter(lambda item: item['sex'] == 1, groups)) # 
     print('len girls=',len(girls),'len all = ', len(groups))
-    res_string='<H2>сайт</H2>'
-    for man in girls:
-        # печать отладочной информации в консоль - чисто для себя
-        print('https://vk.com/id', man['id'],'  ',str(man.get('first_name')) + ' '+  str(man.get('last_name')), sep='')
-        # картинка
-        res_string+='<img src=\"'
-        res_string+=str(man.get('photo_400_orig'))
-        res_string+='\'>'
-        # перенос
-        res_string+='<br>'
-        # название (параграф)
-        res_string+='<p>'
-        res_string+=str(man.get('first_name')) + ' '+  str(man.get('last_name'))
-        res_string+='</p>'
-        # ссылка
-        res_string+='<a href=\">'
-        res_string+='https://vk.com/id'
-        res_string+= str(man['id'])
-        res_string+='\'>https://vk.com/id'
-        res_string+=str(man['id'])
-        res_string+='</a>'
-        # перенос
-        res_string+='<br>'
-        # имя (параграф)
-        res_string+='  <p>'
-        res_string+=str(man.get('first_name'))
-        res_string+=str(man.get('last_name'))
-        res_string+='</p>'
-        res_string+='<br>'
-    return render_template_string(res_string)
+    context=girls 
+    return render_template('getMembers.html', context=context)
 
 if __name__ == '__main__':
     app.run(debug=True)
